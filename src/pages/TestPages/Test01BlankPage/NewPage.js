105fcb1547641cae81ef285cc6407813801609c3
Bu Qiang <yoyobq@hotmail.com>
2020-04-26 14:25:34 +0800

    更新了Test01中的代码与注释，增加对JSX的理解

--- src/pages/TestPages/Test01BlankPage/NewPage.js
+++ src/pages/TestPages/Test01BlankPage/NewPage.js
@@ -1,10 +1,21 @@
+// 必须引用React
+import React from 'react';
 import styles from './NewPage.less';
-// 这是一个简单的页面
+
+// 这是一个简单的ADP页面
 // 主要参考了 https://pro.ant.design/docs/new-page-cn 描述完成
-export default() => {
-  // return <div class={styles.testStyle}>Test01 New Blank Page</div>
+const name = 'Alex';
+const element = (
+  <div id="root">
+    <h1>hello, {name}</h1>
+    <div className={styles.testStyle}>Test01 New Blank Page</div>
+  </div>
+);
+
+export default () => {
+  // return <div className={styles.testStyle}>Test01 New Blank Page</div>
   return element;
-}
+};
 
 // 上述页面没有讨论路由的添加方式在此一并记录
 // 注意这是从ADP框架反推来学习的，
@@ -24,10 +35,13 @@ export default() => {
 // 最后一题同名的.less样式，似乎没法为本页面默认加载CSS样式（基于200417的理解）
 // 根据 welcome.less 和 welcom.tsx 页面中的css引用形式修改了代码，成功引入了css样式
 
-const name = 'Alex';
-const element = <h1>hello, {name}</h1>;
+// 提供一个非ADP框架下的例子
+// 该行需在 import styles 前
+// import ReactDOM from 'react-dom';
 
 // ReactDOM.render(
+//   // 要注意element中定义的 id='root'
+//   element,
 //   element,
 //   document.getElementById('root')
 // );
