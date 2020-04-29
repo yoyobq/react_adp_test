c0e2d2aa2eba230c892356cceb22bcbe667431cd
Bu Qiang@SSTS <yoyobq@hotmail.com>
2020-04-29 13:07:01 +0800

    git commit出错，调试3

--- src/pages/TestPages/Test04State&Lifecycle/State&Lifecycle.tsx
+++ src/pages/TestPages/Test04State&Lifecycle/State&Lifecycle.tsx
@@ -44,23 +44,20 @@ class Clock extends React.Component {
 
   // (3)利用lifecycle完成计时更新
   // 当 Clock 组件第一次被渲染到 DOM 中的时候，就为其设置一个计时器。这在 React 中被称为“挂载（mount）”。
-  // componentDidMount() {
-  //   this.timerID = setInterval(
-  //     () => this.tick(),
-  //     1000
-  //   );
-  // }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
 
-  // // 同时，当 DOM 中 Clock 组件被删除的时候，应该清除计时器。这在 React 中被称为“卸载（unmount）”。
-  // componentWillUnmount() {
-  //   clearInterval(this.timerID);
-  // }
+  // 同时，当 DOM 中 Clock 组件被删除的时候，应该清除计时器。这在 React 中被称为“卸载（unmount）”。
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-  // tick() {
-  //   this.setState({
-  //     date: new Date()
-  //   });
-  // }
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
 
   render() {
     return (
