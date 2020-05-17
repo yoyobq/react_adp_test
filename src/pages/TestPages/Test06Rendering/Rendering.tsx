68b01ee088be81eba6d0d46035151812ea5c4708
Bu Qiang <yoyobq@hotmail.com>
2020-05-17 22:39:23 +0800

    Test06中实现条件渲染之&&、三目运算符和中止渲染的示例

--- src/pages/TestPages/Test06Rendering/Rendering.tsx
+++ src/pages/TestPages/Test06Rendering/Rendering.tsx
@@ -86,12 +86,71 @@ class Greeting extends React.Component {
   }
 }
 
+class Greeting2 extends React.Component {
+  props!: {
+    isShown: boolean;
+  };
+
+  state = {
+    isShown: true,
+  };
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isShown: props.isShown,
+    };
+
+    // this.handleBtnClick = this.handleBtnClick.bind(this);
+  }
+
+  render() {
+    const { isShown } = this.state;
+    return (
+      // 注意外层的div和三目运算符转弯的写法
+      <div>{isShown ? <Welcome name="Welcome Alex!" /> : <Hello data="Hello, Friend." />}</div>
+    );
+  }
+}
+
+function Mailbox(props: any) {
+  // 此处请注意下面的写法会引起eslint报错，强制使用es6的解构赋值
+  // const unreadMessages = props.unreadMessages;
+  const { unreadMessages } = props;
+  return (
+    <div>
+      {unreadMessages.length > 0 && <h4>You have {unreadMessages.length} unread messages.</h4>}
+    </div>
+  );
+}
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+// 以下是一段利用直接return null终止渲染的示例代码，
+// 由于代码本身逻辑简单，就是强行return，就不再页面内实现了（实现了也看不着）
+// function WarningBanner(props) {
+//   if (!props.warn) {
+//     return null;
+//   }
+
+//   return (
+//     <div className="warning">
+//       Warning!
+//     </div>
+//   );
+// }
+
 export default (): React.ReactNode => (
   <PageHeaderWrapper>
     {/* 这里有个小坑，boolean的props是不能写成 isShown={true} 而应该直接写 isShown 即可否则git commit的时候报错 */}
+    <h2>基本例子，代码里的Greeting部分</h2>
     <Greeting isShown={false} />
     {/* 原本想结合Test05的内容做一个可以切换显示的按钮，发现还需要后面的知识，暂时留空 */}
-    {/* 此按钮暂时无效 */}
+    <div>此按钮暂时无效</div>
     <Toggle />
+    <hr />
+    <h2>与运算符的例子，代码里的&&</h2>
+    <Mailbox unreadMessages={messages} />
+    <hr />
+    <Greeting2 isShown />
   </PageHeaderWrapper>
 );
