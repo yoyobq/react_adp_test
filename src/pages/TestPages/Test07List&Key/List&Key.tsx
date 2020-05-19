33fafcff7badb37a20d88e76521f88d5870cecdb
Bu Qiang <yoyobq@hotmail.com>
2020-05-19 23:14:48 +0800

    Test07新增class形式的list

--- src/pages/TestPages/Test07List&Key/List&Key.tsx
+++ src/pages/TestPages/Test07List&Key/List&Key.tsx
@@ -3,12 +3,11 @@ import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout'; // 自动生成页面头部的map链接
 // import styles from './List&Key.less';
 
+const arr = [1, 2, 'c', 4, 'e'];
 // 定义返回值类型是由 JSX的Element 组成的数组
 function List(): Array<JSX.Element> {
-  const numbers = [1, 2, 'c', 4, 'e'];
-
   // map() 方法返回一个新数组，数组中的元素为原始数组元素调用函数处理后的值。
-  const listItems = numbers.map((number, index) => <li>{`${index} - ${number}`}</li>);
+  const listItems = arr.map((item, index) => <li>{`${index} - ${item}`}</li>);
 
   // 如果直接返回ListItems的话，会出一个很有意思的错误
   // 渲染时候<List /> 会报告:
@@ -34,22 +33,34 @@ const OrderList: React.FC<{}> = () => {
   );
 };
 
-// class Hello extends React.Component {
-//   state = {
-//     data: null,
-//   };
+// 用class方法实现同样的list，并提供可替换属性list
+// 请注意，这里使用了自定义属性list，处理方式和之前的例子不同
+// 之前是对JSX的定义进行操作，现在是用自定义接口对当前的class进行限制
+// 显然是这里的方法更好，以后都会使用这样的方式
+interface Props {
+  list: Array<any>;
+}
 
-//   constructor(props: any) {
-//     super(props);
-//     this.state.data = props['data-data'];
-//   }
+class UnOrderList extends React.Component<Props, {}> {
+  state = {
+    arrList: [],
+  };
 
-//   render() {
-//     return <div className={styles['test-Style']}>{this.state.data}</div>;
-//   }
-// }
+  constructor(props: any) {
+    super(props);
+    this.state.arrList = props.list.map((item: any, index: number) => (
+      <li>{`${index} - ${item}`}</li>
+    ));
+  }
+
+  render() {
+    return <ul>{this.state.arrList}</ul>;
+  }
+}
+
 export default (): React.ReactNode => (
   <PageHeaderWrapper>
     <OrderList />
+    <UnOrderList list={arr} />
   </PageHeaderWrapper>
 );
