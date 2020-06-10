224ad785dd92b7b8a012d93c9d6966991b5b0d81
Bu Qiang@SSTS <yoyobq@hotmail.com>
2020-06-10 16:30:07 +0800

    基本完成DvaTest02

--- src/pages/DvaTest/Dva02/index.tsx
+++ src/pages/DvaTest/Dva02/index.tsx
@@ -1,67 +1,10 @@
-import { Button, Popconfirm, Table } from 'antd';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import React from 'react';
-import { connect, Dispatch } from 'umi';
-// import { PageHeaderWrapper } from '@ant-design/pro-layout';
-// import ProductList from '@/components/ProductList';
-// import React, { useState, useEffect } from 'react';
-// import styles from './index.less';
-// import products from './model/model';
-import { ProductsStateType } from './model/model';
+import styles from './index.less';
+import ProductList from './Products';
 
-interface ProductsProps  {
-  dispatch: Dispatch,
-  products: ProductsStateType,
-};
-
-const Products: React.FC<ProductsProps> = ({dispatch, products}) => {
-// 同样的定义，两种写法，保留供参考
-//  const Products: React.FC<ProductsProps> = (props) => {
-//  const { dispatch, products } = props;
-
-  function handleDelete(id: string) {
-    dispatch({
-      type: 'products/delete',
-      payload: id,
-    });
-  }
-  return (
-    <div>
-      <h2>List of Products</h2>
-      <ProductList onDelete={handleDelete} products={products} />
-    </div>
-  );
-};
-
-interface ProductsListProps  {
-  onDelete: Function,
-  products: any,
-};
-
-const ProductList : React.FC<ProductsListProps>= ({ onDelete, products }) => {
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-    },
-    {
-      title: 'Actions',
-      render: (text, record) => {
-        return (
-          <Popconfirm title="Delete?" onConfirm={() => onDelete(record.id)}>
-            <Button>Delete</Button>
-          </Popconfirm>
-        );
-      },
-    },
-  ];
-  return <Table dataSource={products} columns={columns} />;
-};
-
-export default connect(({ products } : ProductsProps ) => ({
-  products,
-}))(Products);
-// export default (): React.ReactNode => (
-//   <PageHeaderWrapper className={styles.main}>
-//     <ProductList />
-//   </PageHeaderWrapper>
-// );
+export default (): React.ReactNode => (
+  <PageHeaderWrapper className={styles.main}>
+    <ProductList />
+  </PageHeaderWrapper>
+);
--- src/pages/DvaTest/Dva02/model/model.ts
+++ src/pages/DvaTest/Dva02/model/model.ts
@@ -1,22 +1,22 @@
 import { Reducer } from 'umi';
 
 // 定义ProductsState的类型
-export interface ProductsStateType {
-    id: string;
-    name: string;
-};
+export interface ProductStateType {
+  id: string;
+  name: string;
+}
 
 // 定义此Model的类型
 export interface ProductsModelType {
   // 命名空间是string
   namespace: string;
-  // state是个ProductsStateType组成的数组
-  state: Array<ProductsStateType>;
+  // state是个ProductStateType组成的数组
+  state: Array<ProductStateType>;
   // reducers描述发生了什么事
   reducers: {
-    delete: Reducer<Array<ProductsStateType>>;
-    // add: Reducer<Array<ProductsStateType>>;
-  }
+    delete: Reducer<Array<ProductStateType>>;
+    // add: Reducer<Array<ProductStateType>>;
+  };
 }
 
 const Model: ProductsModelType = {
@@ -24,16 +24,16 @@ const Model: ProductsModelType = {
   // 此处的state数据是直接赋值了，
   // 以后应该是通过service从后台获取
   state: [
-    { name: 'dva', id: 'dva' },
-    { name: 'antd', id: 'antd' },
-    { name: 'react', id: 'react' },
+    { name: 'dva4', id: 'dva1' },
+    { name: 'antd', id: 'antd2' },
+    { name: 'react', id: 'react3' },
   ],
   reducers: {
     delete(state, { payload: id }) {
       // state可能是undfiened，所以要!
       // 返回所有和id不符的数据
-      return state!.filter(item => item.id !== id);
-    }
+      return state!.filter((item) => item.id !== id);
+    },
     // add(state, { payload }) {
     //   return state?.push({name: 'addtest', id: 'addtest'});
     // }
